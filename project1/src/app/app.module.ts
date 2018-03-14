5cbe8a6f6855188ee9daf3a2a1d5419792e25209
prabhuhosamani <prabhuhosamani9@gmail.com>
2018-03-14 09:39:20 +0530

    Created Servers Component

--- project1/src/app/app.module.ts
+++ project1/src/app/app.module.ts
@@ -4,11 +4,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,ServerComponent
+    AppComponent,ServerComponent, ServersComponent
   ],
   imports: [
     BrowserModule
