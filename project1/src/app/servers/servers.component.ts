46fb0697e71067c0f6796020b127830383c870c6
prabhuhosamani <prabhuhosamani9@gmail.com>
2018-03-14 12:52:16 +0530

    property binding over string interpolation

--- project1/src/app/servers/servers.component.ts
+++ project1/src/app/servers/servers.component.ts
@@ -2,12 +2,17 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
+ // template: '<app-server></app-server><app-server></app-server>',
+ templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+   allowNewSever= false;
+  constructor() { 
+    setTimeout(() =>{
+      this.allowNewSever = true;
+    },2000);
+  }
 
   ngOnInit() {
   }
